82041095616f2e7350172bcdfe05100ed304f9ca
Cian Hughes <chughes000@gmail.com>
2024-05-13 15:29:55 +0100

    Created consistent dataloader

---
+++ symbolic_nn_tests/dataloader.py
@@ -0,0 +1,11 @@
+from torchvision.datasets import Caltech256
+from torch.utils.data import random_split
+from torch.utils.data import BatchSampler
+
+
+def get_dataset(split: (float, float, float) = (0.7, 0.1, 0.2), *args, **kwargs):
+    ds = Caltech256("../datasets/", download=True)
+    train, test, val = (
+        BatchSampler(i, *args, **kwargs) for i in random_split(ds, split)
+    )
+    return train, test, val
