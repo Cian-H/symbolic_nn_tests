Fixed similarity matrix normalization

symbolic_nn_tests/model.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from torch import nn
 
 
@@ -8,11 +9,19 @@ model = nn.Sequential(
 )
 
 
-def main(loss_func=nn.functional.cross_entropy, logger=None):
+# This is just a quick, lazy way to ensure all models are trained on the same dataset
+@lru_cache(maxsize=1)
+def get_singleton_dataset():
     from torchvision.datasets import QMNIST
-    import lightning as L
 
     from .dataloader import get_dataset
+
+    return get_dataset(dataset=QMNIST)
+
+
+def main(loss_func=nn.functional.cross_entropy, logger=None):
+    import lightning as L
+
     from .train import TrainingWrapper
 
     if logger is None:
@@ -20,8 +29,8 @@ def main(loss_func=nn.functional.cross_entropy, logger=None):
 
         logger = TensorBoardLogger(save_dir=".", name="logs/ffnn")
 
-    train, val, test = get_dataset(dataset=QMNIST)
-    lmodel = TrainingWrapper(model)
+    train, val, test = get_singleton_dataset()
+    lmodel = TrainingWrapper(model, loss_func=loss_func)
     trainer = L.Trainer(max_epochs=5, logger=logger)
     trainer.fit(model=lmodel, train_dataloaders=train, val_dataloaders=val)
 

symbolic_nn_tests/train.py
@@ -22,7 +22,8 @@ class TrainingWrapper(L.LightningModule):
         x, y = collate_batch(batch)
         y_pred = self.model(x)
         batch_size = x.shape[0]
-        loss = self.loss_func(y_pred, nn.functional.one_hot(y).type(torch.float64))
+        one_hot_y = nn.functional.one_hot(y).type(torch.float64)
+        loss = self.loss_func(y_pred, one_hot_y)
         acc = torch.sum(y_pred.argmax(dim=1) == y) / batch_size
         self.log(f"{label}{'_' if label else ''}loss", loss, batch_size=batch_size)
         self.log(f"{label}{'_' if label else ''}acc", acc, batch_size=batch_size)