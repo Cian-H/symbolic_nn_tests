15a57cc22924ed60ca0ed67c3756dd9cebb71fda
Cian-H <chughes000@gmail.com>
2024-05-15 13:37:29 +0100

    Added wandb logging

--- symbolic_nn_tests/__main__.py
+++ symbolic_nn_tests/__main__.py
@@ -4,14 +4,16 @@ LEARNING_RATE = 10e-5
 def run_test(loss_func, version):
     from .model import main as test_model
     from lightning.pytorch.loggers import TensorBoardLogger
+    from lightning.pytorch.loggers import WandbLogger
 
-    logger = TensorBoardLogger(
+    tb_logger = TensorBoardLogger(
         save_dir=".",
         name="logs/comparison",
         version=version,
     )
-    test_model(lr=LEARNING_RATE)
-    # test_model(logger=logger, loss_func=loss_func, lr=LEARNING_RATE)
+    wandb_logger = WandbLogger(project="MNIST")
+    logger = [tb_logger, wandb_logger]
+    test_model(logger=logger, loss_func=loss_func, lr=LEARNING_RATE)
 
 
 def main():
@@ -19,10 +21,10 @@ def main():
     from torch import nn
 
     run_test(nn.functional.cross_entropy, "cross_entropy")
-    # run_test(semantic_loss.similarity_cross_entropy, "similarity_cross_entropy")
-    # run_test(semantic_loss.hasline_cross_entropy, "hasline_cross_entropy")
-    # run_test(semantic_loss.hasloop_cross_entropy, "hasloop_cross_entropy")
-    # run_test(semantic_loss.multisemantic_cross_entropy, "multisemantic_cross_entropy")
+    run_test(semantic_loss.similarity_cross_entropy, "similarity_cross_entropy")
+    run_test(semantic_loss.hasline_cross_entropy, "hasline_cross_entropy")
+    run_test(semantic_loss.hasloop_cross_entropy, "hasloop_cross_entropy")
+    run_test(semantic_loss.multisemantic_cross_entropy, "multisemantic_cross_entropy")
 
 
 if __name__ == "__main__":
