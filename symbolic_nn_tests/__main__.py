2720887f8806cfd105eb8869a1b4e7d94ecc5d6f
Cian Hughes <chughes000@gmail.com>
2024-05-23 11:06:11 +0100

    Added cli for configuring/launching experiments

--- symbolic_nn_tests/__main__.py
+++ symbolic_nn_tests/__main__.py
@@ -1,20 +1,31 @@
+import typer
 from typing import Optional, Iterable
-import experiment1
+from typing_extensions import Annotated
+from . import experiment1
 
 
 EXPERIMENTS = (experiment1,)
 
 
-def main(
-    experiments: Optional[int | Iterable[int]] = None,
-    tensorboard: bool = True,
-    wandb: bool = True,
-):
-    if experiments is None:
-        experiments = range(1, len(EXPERIMENTS) + 1)
-    elif not isinstance(experiments, Iterable):
-        experiments = (experiments,)
+def parse_int_or_intiterable(i: Optional[str]) -> Iterable[int]:
+    return range(1, len(EXPERIMENTS) + 1) if i is None else map(int, i.split(","))
 
+
+def main(
+    experiments: Annotated[
+        Optional[str],
+        typer.Option(
+            parser=parse_int_or_intiterable,
+            help="A comma separated list of experiments to be run. Defaults to all.",
+        ),
+    ] = None,
+    tensorboard: Annotated[
+        bool, typer.Option(help="Whether or not to log via tensorboard")
+    ] = True,
+    wandb: Annotated[
+        bool, typer.Option(help="Whether or not to log via Weights & Biases")
+    ] = True,
+):
     experiment_indeces = (i - 1 for i in experiments)
     experiment_funcs = [EXPERIMENTS[i].run for i in experiment_indeces]
 
@@ -23,4 +34,4 @@ def main(
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
